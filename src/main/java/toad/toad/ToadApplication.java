3f3d67c3a4c3d1844ec47d3cf9c0824cd02dda0e
eun0901 <jk2310400@ajou.ac.kr>
2024-01-21 22:22:07 +0900

    springboot init

---
+++ src/main/java/toad/toad/ToadApplication.java
@@ -0,0 +1,13 @@
+package toad.toad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toad/toad/ToadApplicationTests.java
@@ -0,0 +1,13 @@
+package toad.toad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
