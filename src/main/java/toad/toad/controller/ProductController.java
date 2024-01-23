d9ed6dffe300749a45ced963f3ff0ea40bdd60f8
juuns <juuns@ajou.ac.kr>
2024-01-24 00:51:28 +0900

    feat: add ProductController

---
+++ src/main/java/toad/toad/controller/ProductController.java
@@ -0,0 +1,27 @@
+package toad.toad.controller;
+
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import toad.toad.dto.ProductSimpleDto;
+import toad.toad.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+}
