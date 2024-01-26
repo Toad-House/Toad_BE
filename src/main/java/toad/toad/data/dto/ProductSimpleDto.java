2b082e4be7462f8e24c02967c6e7b5e5554030f0
juuns <juuns@ajou.ac.kr>
2024-01-27 07:33:56 +0900

    chore: add annotation

--- src/main/java/toad/toad/data/dto/ProductSimpleDto.java
+++ src/main/java/toad/toad/data/dto/ProductSimpleDto.java
@@ -1,12 +1,15 @@
 package toad.toad.data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProductSimpleDto {
     private int productId;
     private String productName;
     private double productPrice;
     private String imageUrl;
+    private int companyId;
     private String companyName;
 }
