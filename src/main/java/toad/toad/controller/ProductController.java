55ffb31264f0fc976ec35403f85203cd419400cd
juuns <juuns@ajou.ac.kr>
2024-01-27 08:09:09 +0900

    feat(api): implement product-search by keyword

--- src/main/java/toad/toad/controller/ProductController.java
+++ src/main/java/toad/toad/controller/ProductController.java
@@ -1,13 +1,11 @@
 package toad.toad.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import toad.toad.data.dto.ProductSimpleDto;
 import toad.toad.service.ProductService;
 
@@ -24,4 +22,16 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "전체 상품 조회", description = "전체 상품 리스트를 조회하는 api 입니다.")
+    @GetMapping
+    public ResponseEntity<List<ProductSimpleDto>> getAllProducts() {
+        List<ProductSimpleDto> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProductSimpleDto>> searchProducts(@RequestParam String keyword) {
+        List<ProductSimpleDto> products = productService.findProductsByKeywords(keyword);
+        return ResponseEntity.ok(products);
+    }
 }
