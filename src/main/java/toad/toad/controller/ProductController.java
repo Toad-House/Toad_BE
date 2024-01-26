1202815404cc429c799bf0c8e78190fc33e16d8a
juuns <juuns@ajou.ac.kr>
2024-01-27 05:10:15 +0900

    Copy data folder to feature/entity branch

--- src/main/java/toad/toad/controller/ProductController.java
+++ src/main/java/toad/toad/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import toad.toad.dto.ProductSimpleDto;
+import toad.toad.data.dto.ProductSimpleDto;
 import toad.toad.service.ProductService;
 
 import java.util.List;
