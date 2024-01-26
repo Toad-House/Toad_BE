e9271e5f90c8029fa4c99c7874b618ff544878e7
juuns <juuns@ajou.ac.kr>
2024-01-27 03:56:28 +0900

    feat: add UserPointUsage entity

---
+++ src/main/java/toad/toad/domain/UserPointUsage.java
@@ -0,0 +1,31 @@
+package toad.toad.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class UserPointUsage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uPointUsageId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private boolean receiveOrUse;   // receive: 0, use: 1
+
+    @ManyToOne
+    @JoinColumn(name = "companyId")
+    private Company company;
+
+    @Column(nullable = false)
+    private int point;  // 받거나 사용한 포인트량
+}
