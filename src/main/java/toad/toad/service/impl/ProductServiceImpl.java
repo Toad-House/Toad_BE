ef3ea98133b03799fb5b310f08c6613b0892ef71
juuns <juuns@ajou.ac.kr>
2024-01-27 10:31:24 +0900

    feat: implement ProductService

--- src/main/java/toad/toad/service/impl/ProductServiceImpl.java
+++ src/main/java/toad/toad/service/impl/ProductServiceImpl.java
@@ -10,8 +10,8 @@ import toad.toad.repository.CompanyRepository;
 import toad.toad.repository.ProductRepository;
 import toad.toad.service.ProductService;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,13 +33,7 @@ public class ProductServiceImpl implements ProductService {
         Company company = companyRepository.findByCompanyName(productDetailDto.getCompanyName())
                                         .orElseThrow(() -> new Exception("company not found"));
 
-        Product newProduct = Product.builder()
-                .productName(productDetailDto.getProductName())
-                .productPrice(productDetailDto.getProductPrice())
-                .productDesc(productDetailDto.getProductDesc())
-                .imageUrl(productDetailDto.getImageUrls().getBytes())
-                .company(company)
-                .build();
+        Product newProduct = modelMapper.map(productDetailDto, Product.class);
         productRepository.save(newProduct);
 
         return newProduct.getProductId();
@@ -61,17 +55,34 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public List<ProductDetailDto> getProductDetail() {
-        return null;
+    public Optional<ProductDetailDto> getProductDetail(int id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.map(product1 -> modelMapper.map(product, ProductDetailDto.class));
     }
 
     @Override
-    public int updateProduct(ProductDetailDto productDetailDto, int id) {
-        return 0;
+    public int updateProduct(ProductDetailDto productDetailDto, int productId) throws Exception {
+        Optional<Product> productOptional = productRepository.findById(productId);
+
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            Company company = companyRepository.findById(productDetailDto.getCompanyId())
+                                .orElseThrow(() -> new Exception("Company not found"));
+
+            product.setProductName(productDetailDto.getProductName());
+            product.setProductPrice(productDetailDto.getProductPrice());
+            product.setProductDesc(productDetailDto.getProductDesc());
+            product.setImageUrl(productDetailDto.getImageUrls().getBytes());
+            product.setCompany(company);
+
+            return productRepository.save(product).getProductId();
+        } else {
+            throw new Exception("Product not found");
+        }
     }
 
     @Override
-    public int deleteProduct(int id) {
-        return 0;
+    public void deleteProduct(int productId) {
+        productRepository.deleteById(productId);
     }
 }
