3d1ee1f36b54ed5b1ed8a5805965ff2552626cdf
juuns <juuns@ajou.ac.kr>
2024-01-27 16:39:02 +0900

    chore: add annotation

--- src/main/java/toad/toad/repository/OrderRepository.java
+++ src/main/java/toad/toad/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package toad.toad.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import toad.toad.data.entity.Order;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findByUser_UserId(int userId);
 }
