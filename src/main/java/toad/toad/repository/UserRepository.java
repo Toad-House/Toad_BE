7f6e23c9b3b613afc48cdde32ab23cf88d290490
juuns <juuns@ajou.ac.kr>
2024-01-27 14:19:30 +0900

    feat: add UserRepository

---
+++ src/main/java/toad/toad/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package toad.toad.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import toad.toad.data.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String username);
+}
