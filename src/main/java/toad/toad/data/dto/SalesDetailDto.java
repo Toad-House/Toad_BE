8f241415ec4212a5fade82efc98ec343e87bf9d3
juuns <juuns@ajou.ac.kr>
2024-01-27 22:24:55 +0900

    feat: add SalesDtos

---
+++ src/main/java/toad/toad/data/dto/SalesDetailDto.java
@@ -0,0 +1,17 @@
+package toad.toad.data.dto;
+
+import java.time.LocalDateTime;
+
+public class SalesDetailDto {
+    private int companyId;
+    private String companyName;
+    private int orderId;
+    private int userId;
+    private int userName;
+    private int productId;
+    private String productName;
+    private int productPrice;
+    private String imageUrl;
+    private int orderNum;
+    private LocalDateTime createdTime;
+}
---
+++ src/main/java/toad/toad/data/dto/SalesSimpleDto.java
@@ -0,0 +1,12 @@
+package toad.toad.data.dto;
+
+import java.time.LocalDateTime;
+
+public class SalesSimpleDto {
+    private int companyId;
+    private String companyName;
+    private int productId;
+    private String productName;
+    private int productPrice;
+    private int salesQuantity;
+}
