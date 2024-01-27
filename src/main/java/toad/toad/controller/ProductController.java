8678583702638b4284afdffa206e4678dc17276a
juuns <juuns@ajou.ac.kr>
2024-01-27 12:29:09 +0900

    feat(api): implement createProduct #5

--- src/main/java/toad/toad/controller/ProductController.java
+++ src/main/java/toad/toad/controller/ProductController.java
@@ -22,6 +22,15 @@ public class ProductController {
         this.productService = productService;
     }
 
+    // CREATE
+    @Operation(summary = "새 상품 등록", description = "새로운 상품을 추가하는 api 입니다.")
+    @PostMapping("/new")
+    public ResponseEntity<Integer> createProduct(@RequestBody ProductDetailDto productDetailDto) throws Exception {
+        int productId = productService.saveProduct(productDetailDto);
+        return ResponseEntity.ok(productId);
+    }
+
+    // READ
     @Operation(summary = "전체 상품 조회", description = "전체 상품 리스트를 조회하는 api 입니다.")
     @GetMapping
     public ResponseEntity<List<ProductSimpleDto>> getAllProducts() {
@@ -29,13 +38,16 @@ public class ProductController {
         return ResponseEntity.ok(products);
     }
 
+    @Operation(summary = "상품 검색", description = "키워드로 상품을 검색하는 api 입니다.")
     @GetMapping("/search")
     public ResponseEntity<List<ProductSimpleDto>> searchProducts(@RequestParam String keyword) {
         List<ProductSimpleDto> products = productService.findProductsByKeywords(keyword);
         return ResponseEntity.ok(products);
     }
 
-    @GetMapping("/{productId}")
+
+    @Operation(summary = "특정 상품 조회", description = "특정 상품의 상세 정보를 조회하는 api 입니다.")
+    @GetMapping("/detail/{productId}")
     public ResponseEntity<?> getProductDetail(@PathVariable int productId) {
         Optional<ProductDetailDto> productDetailOptional = productService.getProductDetail(productId);
         return productDetailOptional.map((ResponseEntity::ok))
