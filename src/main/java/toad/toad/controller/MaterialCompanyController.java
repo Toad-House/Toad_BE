b61e1b257eeb8e90be96faa84ae5d927b69dbec5
eun0901 <jk2310400@ajou.ac.kr>
2024-02-05 01:37:46 +0900

    Feat: materialComanyController 추가

--- src/main/java/toad/toad/controller/MaterialCompanyController.java
+++ src/main/java/toad/toad/controller/MaterialCompanyController.java
@@ -1,9 +1,17 @@
 package toad.toad.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import toad.toad.data.dto.RequestCompanyDto;
 import toad.toad.service.MaterialCompanyService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/company")
@@ -15,5 +23,11 @@ public class MaterialCompanyController {
         this.materialCompanyService = materialCompanyService;
     }
 
+    @Operation(summary="재료 제공 요청 보기", description = "회사의 마이페이지에서 유저들이 회사로 재료 제공 요청을 보낸 list들을 볼 수 있습니다.")
+    @GetMapping("/request")
+    public ResponseEntity<List<RequestCompanyDto>> showAllRequest (@RequestBody Integer companyId) throws Exception {
+
+        return ResponseEntity.status(HttpStatus.OK).body();
+    }
 
 }
--- src/main/java/toad/toad/service/impl/MaterialCompanyServiceImpl.java
+++ src/main/java/toad/toad/service/impl/MaterialCompanyServiceImpl.java
@@ -3,4 +3,5 @@ package toad.toad.service.impl;
 import toad.toad.service.MaterialCompanyService;
 
 public class MaterialCompanyServiceImpl implements MaterialCompanyService {
+
 }
