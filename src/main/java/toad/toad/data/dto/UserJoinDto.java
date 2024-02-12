73076987cadb120dbe155249536ded0b605817b0
juuns <juuns@ajou.ac.kr>
2024-02-12 17:47:06 +0900

    feat: add joinDtos

---
+++ src/main/java/toad/toad/data/dto/UserJoinDto.java
@@ -0,0 +1,10 @@
+package toad.toad.data.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserJoinDto {
+    private String userName;
+    private String password;
+    private String userContact;
+}
