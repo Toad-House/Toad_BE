8c4dbacc054d9dafaf1f4540b067787ebddebb6a
juuns <juuns@ajou.ac.kr>
2024-02-13 16:29:17 +0900

    fix: variable type

--- src/main/java/toad/toad/data/dto/OrderGetDto.java
+++ src/main/java/toad/toad/data/dto/OrderGetDto.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 public class OrderGetDto {
     private int orderId;
     private int userId;
-    private int userName;
+    private String userName;
     private int productId;
     private String productName;
     private double productPrice;
