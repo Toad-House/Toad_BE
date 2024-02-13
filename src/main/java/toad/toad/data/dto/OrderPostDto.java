b77598f2c6dfd95e2d37295dbbcb560710a8cf29
juuns <juuns@ajou.ac.kr>
2024-02-13 17:15:32 +0900

    chore: add annotation

--- src/main/java/toad/toad/data/dto/OrderPostDto.java
+++ src/main/java/toad/toad/data/dto/OrderPostDto.java
@@ -1,8 +1,10 @@
 package toad.toad.data.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class OrderPostDto {
     private int userId;
     private int productId;
