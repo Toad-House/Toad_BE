1d6bb8c725f2ffa3d8e8ebb51f6a4e10e638d013
juuns <juuns@ajou.ac.kr>
2024-02-14 15:14:16 +0900

    fix: SQL error

--- src/main/java/toad/toad/repository/OrderRepository.java
+++ src/main/java/toad/toad/repository/OrderRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import toad.toad.data.entity.Order;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +15,6 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> findByProduct_ProductId(int productId);
 
-    @Query("SELECT o.product.productId, SUM(o.orderNum) FROM Order o WHERE o.product.productId = :productId GROUP BY o.product.productId")
+    @Query("SELECT SUM(o.orderNum) FROM Order o WHERE o.product.productId = :productId GROUP BY o.product.productId")
     Optional<Integer> getTotalSalesQuantityByProductId(@Param("productId") int productId);
 }
