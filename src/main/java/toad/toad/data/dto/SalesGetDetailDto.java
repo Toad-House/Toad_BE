a90090da469a08801f7b6411dd438142d935b5ea
juuns <juuns@ajou.ac.kr>
2024-02-14 16:19:13 +0900

    refactor: simplify SalesDtos

--- src/main/java/toad/toad/data/dto/SalesGetDetailDto.java
+++ src/main/java/toad/toad/data/dto/SalesGetDetailDto.java
@@ -8,15 +8,15 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class SalesGetDetailDto {
-    private int companyId;
-    private String companyName;
+//    private int companyId;
+//    private String companyName;
     private int orderId;
     private int userId;
     private String userName;
-    private int productId;
-    private String productName;
-    private int productPrice;
-    private String imageUrl;
+//    private int productId;
+//    private String productName;
+//    private int productPrice;
+//    private String imageUrl;
     private int orderNum;
     private LocalDateTime createdTime;
 }
--- src/main/java/toad/toad/data/dto/SalesGetSimpleDto.java
+++ src/main/java/toad/toad/data/dto/SalesGetSimpleDto.java
@@ -6,8 +6,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class SalesGetSimpleDto {
-    private int companyId;
-    private String companyName;
+//    private int companyId;
+//    private String companyName;
     private int productId;
     private String productName;
     private int productPrice;
