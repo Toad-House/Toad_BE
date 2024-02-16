c627dad9a86b351b405510e5f1d731d68b14fb4e
juuns <juuns@ajou.ac.kr>
2024-02-16 20:19:25 +0900

    fix: CORS

--- src/main/java/toad/toad/config/WebConfig.java
+++ src/main/java/toad/toad/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3000);
